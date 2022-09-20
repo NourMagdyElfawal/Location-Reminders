0967a92fd41909b7fc5ef78844d976a9690603ee
NourMagdyElfawal <noor882005@gmail.com>
2022-09-20 05:58:45 +0200

    test navigation to save reminder fragment pass

--- app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -5,17 +5,25 @@ import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.udacity.project4.R
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.module
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 
@@ -25,7 +33,49 @@ import org.mockito.Mockito.verify
 @MediumTest
 class ReminderListFragmentTest {
 
+
+//    private lateinit var fakeDataSource: FakeDataSource
+    private lateinit var reminderListViewModel: RemindersListViewModel
+
+    @Before
+    fun setup() {
+//        fakeDataSource = FakeDataSource()
+//        reminderListViewModel =
+//            RemindersListViewModel(ApplicationProvider.getApplicationContext(), fakeDataSource)
+//        stopKoin()
+//
+//        val myModule = module {
+//            single {
+//                reminderListViewModel
+//            }
+//        }
+//        // new koin module
+//        startKoin {
+//            modules(listOf(myModule))
+//        }
+    }
+
 //    TODO: test the navigation of the fragments.
+@Test
+fun testNavigationToSaveReminderFragment() {
+    // Create a mock NavController object which has all navController function without any  implementation
+    val mockNavController = mock(NavController::class.java)
+
+     launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+    .onFragment {
+         Navigation.setViewNavController(it.view!!, mockNavController)
+    }
+    // Verify that performing a click prompts the correct Navigation action
+    onView(withId(R.id.addReminderFAB)).perform(click())
+    verify(mockNavController).navigate(
+        ReminderListFragmentDirections.toSaveReminder())
+}
+
+
+
 //    TODO: test the displayed data on the UI.
+
+
+
 //    TODO: add testing for the error messages.
 }
